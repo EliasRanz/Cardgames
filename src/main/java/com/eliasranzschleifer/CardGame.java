1ab9962b87d8cd78e16f8c81da1c851f43ad9ebb
Elias Ranz-Schleifer <elias.ranz-schleifer@dealer.com>
2016-05-13 00:31:28 -0400

    Removed game choice for time being

--- src/main/java/com/eliasranzschleifer/CardGame.java
+++ src/main/java/com/eliasranzschleifer/CardGame.java
@@ -1,28 +1,9 @@
 package com.eliasranzschleifer;
 
-import java.util.Scanner;
-
 public class CardGame {
     public static void main(String[] args) {
-//        GUI gui = new GUI(15,15);
-        Scanner input = new Scanner(System.in);
-        String gameChoice;
-
-
-        System.out.println("What game would you like to play?\nBlackjack (b) ");
-        gameChoice = input.nextLine();
-
         Game game;
-        switch (gameChoice){
-            case "b":
-                game = new BlackJack();
-                System.out.println("You chose blackjack!");
-                break;
-            default:
-                game = new BlackJack();
-                System.out.println("You entered an invalid game. You're playing blackjack!");
-        }
-
+        game = new BlackJack();
         game.play();
         System.out.println(game);
     }
